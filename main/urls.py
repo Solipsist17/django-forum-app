1b342b164dddb945307d5b5fbd3e71160f6f713d
Solipsist17 <sebas.17v@gmail.com>
2024-08-18 23:16:31 -0500

    agregando lógica de autenticación

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
     path("create_category/", views.create_category, name="create_category"),
-    path("categories/", views.categories, name="categories")
+    path("categories/", views.categories, name="categories"),
+    path("logout/", views.signout, name="logout"),
+    path("signin/", views.signin, name="signin"),
+    path("feed/", views.feed, name="feed")
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .models import Category
 from django.http import HttpResponse
@@ -9,6 +9,10 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
+def feed(request):
+    return render(request, 'feed.html')
+
+# registrarse
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html', {
@@ -32,6 +36,32 @@ def signup(request):
                         'form': UserCreationForm,
                         'error': 'Password do not match'
                 })
+
+# cerrar sesión    
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+# iniciar sesión
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], 
+            password=request.POST['password']
+        )
+
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or Password is incorrect'
+            }) 
+        else:
+            login(request, user)
+            return redirect('feed')
     
 def create_category(request):
     Category.objects.create(name='category1', description='description_category1')
